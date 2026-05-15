75b9eaa8e5faa552232df8c4ad80b276411dc7a2
Nicolas <nicolas.lima2108@gmail.com>
2026-05-14 21:24:57 -0300

    Adiciona AppDbContext e configura EF Core com SQLite

--- GameDashboard/Data/GameDashboardContext.cs
+++ GameDashboard/Data/GameDashboardContext.cs
@@ -1,1 +1,23 @@
-﻿
+﻿using GameDashboard.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace GameDashboard.Data;
+
+public class GameDashboardContext : DbContext
+{
+    public DbSet<Jogo> Jogos { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder options)
+        => options.UseSqlite("Data Source=gamedashboard.db");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Jogo>(entity =>
+        {
+            entity.HasKey(j => j.Id);
+            entity.Property(j => j.Nome).IsRequired().HasMaxLength(300);
+            entity.Property(j => j.Slug).HasMaxLength(300);
+            entity.HasIndex(j => j.Slug).IsUnique();
+        });
+    }
+}
