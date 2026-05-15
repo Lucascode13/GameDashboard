9567e40ad13c33663f81fbcd9fe9314c6fb1d829
Nicolas <nicolas.lima2108@gmail.com>
2026-05-14 21:29:16 -0300

    Configura injeção de dependência no App.xaml.cs

--- GameDashboard/App.xaml.cs
+++ GameDashboard/App.xaml.cs
@@ -1,1 +1,29 @@
-﻿
+﻿using System.Windows;
+using GameDashboard.Services;
+using GameDashboard.ViewModels;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace GameDashboard;
+
+public partial class App : Application
+{
+    private readonly ServiceProvider _provider;
+
+    public App()
+    {
+        var services = new ServiceCollection();
+
+        services.AddHttpClient<JogoService>();
+        services.AddTransient<MainViewModel>();
+        services.AddTransient<MainWindow>();
+
+        _provider = services.BuildServiceProvider();
+    }
+
+    protected override void OnStartup(StartupEventArgs e)
+    {
+        base.OnStartup(e);
+        var window = _provider.GetRequiredService<MainWindow>();
+        window.Show();
+    }
+}
